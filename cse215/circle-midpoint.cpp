001bf861305a08fcd7e50de4db070b0c0d07675a
switchkiller <emailbarung@gmail.com>
2015-08-07 16:58:29 +0530

    Circle generation using Midpoint circle generation

---
+++ cse215/circle-midpoint.cpp
@@ -0,0 +1,45 @@
+/*---------------------
+| Author: switchkiller |
+----------------------*/
+#include<iostream>
+#include <stdio.h>
+#include <graphics.h>
+#include <math.h>
+using namespace std;
+void circleMidpoint(int, int, int);
+void circlePlotPoints(int, int, int, int );
+int main(){
+  int xc,yc,r;
+  int gd = DETECT, gm;
+  cout<<"Enter the center and the radius: ";
+  cin>>xc>>yc>>r;
+  initgraph(&gd, &gm, NULL);
+  circleMidpoint(xc, yc, r);
+  getch();
+  closegraph();
+  return 0;
+}
+void circleMidpoint(int xc, int yc, int radius){
+  int x=0, y=radius, p=1-radius;
+  circlePlotPoints(xc,yc,x,y);
+  while(x < y){
+    x++;
+    if (p < 0)
+      p += 2*x+1;
+      else{
+        y--;
+        p += 2*(x-y)+1;
+      }
+      circlePlotPoints(xc,yc,x,y);
+  }
+}
+void circlePlotPoints(int xc, int yc, int x, int y){
+  putpixel (xc+x,yc+y,WHITE);
+  putpixel (xc-x,yc+y,WHITE);
+  putpixel (xc+x,yc-y,WHITE);
+  putpixel (xc-x,yc-y,WHITE);
+  putpixel (xc+y,yc+x,WHITE);
+  putpixel (xc-y,yc+x,WHITE);
+  putpixel (xc+y,yc-x,WHITE);
+  putpixel (xc-y,yc-x,WHITE);
+}
