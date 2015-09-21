bfdcb93f10da12aae1f52b506a3c8345498a4cbd
switchkiller <emailbarung@gmail.com>
2015-09-21 12:33:03 +0530

    Sorting is almost complete

--- cse212/Binarysll.cpp
+++ cse212/Binarysll.cpp
@@ -1,46 +1,79 @@
 #include<iostream>
 #include<cstdio>
-
+#include<stdlib.h>
+#include<stdio.h>
 using namespace std;
 
-typedef struct {
+struct node{
 	int data;
 	struct node* next;
-}node;
+};
 
 
 void insertbeg(struct node **head, int data){
 	struct node *temp;
-	temp = (struct node*) malloc(sizeof(struct node));
+	temp = (struct node* ) malloc(sizeof(struct node));
 	temp->data = data;
-	temp->next = start;
-	start = temp;
+	temp->next = *head;
+	*head = temp;
 }
 
 void insertend(struct node **head,int data){
 	struct node *temp, *p;
 	temp = (struct node*) malloc(sizeof(struct node));
 	temp->data = data;
-	p = start;
+	p = *head;
 	while (p != NULL)
 		p = p->next;
-	temp->next = p;	
+	p->next = temp;
+	temp->next = NULL;	
 }
 
-int binarysearch(struct node *head){
-
+void insertpos(struct node **head,int data){
+	struct node *tmp, *temp;
+	tmp = *head;
+	temp = (struct node*) malloc(sizeof(struct node));
+	temp->data = data;
+	while ((tmp->data < data) && (tmp->next != NULL)){
+		tmp = tmp->next;
+	}
+	if (tmp->next == NULL){
+		temp->next == NULL;
+		tmp->next = temp;
+		return;
+	}
+	temp->next = tmp->next;
+	tmp->next = temp;	
 }
+
+void print(struct node *head){
+	struct node *tmp; 
+	tmp = head;
+	while (tmp != NULL){
+		printf("%d: ",tmp->data);
+		tmp = tmp->next;
+	}
+}
+
+//int binarysearch(struct node *head){
+
+
 
 int main(){
 	struct node *head;
 	head = NULL;
-	int n, data;
+	int n, data, counter = 0;
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
+		counter++;
 		cin >> data;
-		insertclever(struct node **head, data);
+		if (counter == 1)
+			insertbeg(&head, data);
+		else
+			insertpos(&head, data);
 	}
-	int key;
-	cin >> key;
-	binarysearch(head,key);
+	print(head);
+//	int key;
+//	cin >> key;
+//	binarysearch(head,key);
 }
